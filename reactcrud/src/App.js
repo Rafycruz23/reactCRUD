625406ba69ce2f4c93984be0f39e56429a634261
Rafa <rafycruz23@live.com>
2023-08-15 14:37:36 -0400

    first commit

---
+++ reactcrud/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Create from './components/Create';
+
+function App() {
+  return (
+    <div className="main">
+      <div className='header'>
+        React Crud Application
+      </div>
+      <Create/>
+    </div>
+  );
+}
+
+export default App;
