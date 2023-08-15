ce87073c52e08730e25c6cf91de02696d9692652
Rafa <rafycruz23@live.com>
2023-08-15 15:16:08 -0400

    added axios, linked it to the mockapi, data is created

--- reactcrud/src/components/Create.js
+++ reactcrud/src/components/Create.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
+import axios from 'axios'
 
 
 export default function Create() {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [checkbox, setcheckbox] = useState(false)
+    
+    const submitData = ()=> {
+        axios.post('https://64dbc532593f57e435b1665a.mockapi.io/crudData',{
+            firstName,
+            lastName,
+            checkbox,
+        })
+        console.log(firstName,lastName,checkbox)
+    }
+
+    // function verifyData(){
+    //     if (firstName || lastName == ''){
+    //         alert('Enter all field information')
+    //     } else if (checkbox == false){
+    //         alert('You must agree to the Terms and Conditions')
+    //     }
+    // }
+
 
 return(
   <Form className='form'>
     <Form.Field>
       <label>First Name</label>
-      <input placeholder='First Name' />
+      <input placeholder='First Name' onChange={(e)=>setFirstName(e.target.value)}/>
     </Form.Field>
     <Form.Field>
       <label>Last Name</label>
-      <input placeholder='Last Name' />
+      <input placeholder='Last Name' onChange={(e)=>setLastName(e.target.value)}/>
     </Form.Field>
     <Form.Field>
-      <Checkbox label='I agree to the Terms and Conditions' />
+      <Checkbox label='I agree to the Terms and Conditions' onChange={(e)=>setcheckbox(!checkbox)}/>
     </Form.Field>
-    <Button type='submit'>Submit</Button>
+    <Button type='submit' onClick={submitData}>Submit</Button>
   </Form>
 )}
 
